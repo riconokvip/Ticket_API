07a0ad466ef9e5b44b49f2030fe76e8962fef9ff
riconokvip <ricon@okvip.com>
2024-06-14 13:48:53 +0700

    update domain

--- Ticket.Persistence/RedisKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ticket.Persistence
-{
-    public static class RedisKeys
-    {
-        public static string Users(int pageIndex, int pageSize, string textSearch) => $"users_{textSearch}_{pageIndex}_{pageSize}";
-
-        public static string EsUsers = "esusers";
-    }
-}
